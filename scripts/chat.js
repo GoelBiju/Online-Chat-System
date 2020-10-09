5225ab8d249c697fcb88a45060892a9b94613fe2
Goel Biju <goelmolel@hotmail.com>
2020-10-09 15:22:53 +0100

    Week 2 - Add validation to message box to check for text

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,1 +1,12 @@
-function sendMessage(message) {}
+$(function () {
+  // Enable/disable send button ensuring there
+  // message text to send.
+  $("#messageBox").keyup(function () {
+    let currText = $("#messageBox").val();
+    if (currText.length > 0) {
+      $("#sendButton").prop("disabled", false);
+    } else {
+      $("#sendButton").prop("disabled", true);
+    }
+  });
+});
