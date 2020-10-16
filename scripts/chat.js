96ebcbe378a7536d28303d73dc1deb951599f3a7
Goel Biju <goelmolel@hotmail.com>
2020-10-16 15:51:01 +0100

    Workshop 3 - Add basic Message data structure

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,3 +1,14 @@
+// Define message data structure.
+class Message {
+  constructor(user, date, content) {
+    this.user = user;
+    this.date = date;
+    this.content = content;
+
+    // TODO: Extend these properties?
+  }
+}
+
 $(function () {
   // Enable/disable send button ensuring there
   // message text to send.
@@ -8,5 +19,16 @@ $(function () {
     } else {
       $("#sendButton").prop("disabled", true);
     }
+  });
+
+  $("#sendButton").click(function () {
+    // Get appropriate values.
+    let username = "goel@chat.com";
+    let date = new Date();
+    let content = $("#messageBox").val();
+
+    // Create a message.
+    var message = new Message(username, date, content);
+    console.log("Constructed message: ", message);
   });
 });
